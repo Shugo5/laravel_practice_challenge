afb12809af084198d3c8df0d1cab7f2a2b3e1c55
Shugo5 <o.syu.0406@gmail.com>
2019-11-06 21:36:57 +0900

    index.blade.phpの作成

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>予約管理システム</title>
+</head>
+<body>
+<header><h1>システムへのご意見をお聞かせください</h1></header>
+<section>
+    <div>
+        <p>氏名<span>※</span></p>
+        <input type="">
+    </div>
+    <div>
+        <p>性別<span>※</span></p>
+        <input type="">
+    </div>
+    <div>
+        <p>年代<span>※</span></p>
+        <input type="">
+    </div>
+    <div>
+        <p>メールアドレス<span>※</span></p>
+        <input type="">
+    </div>
+    <div>
+        <p>メール送信可否</p>
+        <p>登録したメールアドレスにメールマガジンをお送りしてもよろしいですか？</p>
+        <input type="">
+        <p>送信を許可します</p>
+    </div>
+    <div>
+        <p>ご意見</p>
+        <input type="">
+    </div>
+
+    <div>
+        <p>確認</p>
+        <input type="">
+    </div>
+</section>    
+</body>
+</html>
