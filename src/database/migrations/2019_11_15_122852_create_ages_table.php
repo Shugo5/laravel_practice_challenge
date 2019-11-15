c35c2c49a7a319386893cf33f8033dd4c6cf0ad0
Shugo5 <o.syu.0406@gmail.com>
2019-11-15 22:00:16 +0900

    answers、agesの2つのテーブルを各マイグレーションファイルに分けて作成した

--- src/database/migrations/2019_11_15_122852_create_ages_table.php
+++ src/database/migrations/2019_11_15_122852_create_ages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAgesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ages', function($table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->string('age',20);
+            $table->tinyInteger('sort');
         });
     }
 
@@ -26,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('ages');
     }
 }
